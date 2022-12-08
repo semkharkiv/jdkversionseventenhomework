98dadaa5ac290af4b78f263d86dd05226057a058
abdullayevsema <info.pelikantobacco@gmail.com>
2022-12-08 20:14:11 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte s = 8;
+        short s1 = 9;
+        int s2 = 44;
+        long s3 = 2022;
+        float s4 = 7.77f;
+        double s5 = 6.0;
+        boolean s6 = true;
+
+        System.out.println("byte="+s );
+        System.out.println("short="+s1);
+        System.out.println("int="+s2);
+        System.out.println("long="+s3);
+        System.out.println("float="+s4);
+        System.out.println("double="+s5);
+        System.out.println("boolean="+s6);
+
+
+    }
+
+
+}
